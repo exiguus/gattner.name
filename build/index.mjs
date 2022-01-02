958ebf1beeb6a53941ae8c31f6f3e2f06c3f7dd5
Simon Gattner <github@0x38.de>
2022-01-02 23:22:58 +0100

    feat(page): home use random content

--- build/index.mjs
+++ build/index.mjs
@@ -1,6 +1,6 @@
 import { getFile } from './lib.mjs'
 /*
- * Fetch data from Github
+ * Fetch data from GitLab
  *  GET /projects/:id/repository/files/:file_path
  *  see: https://docs.gitlab.com/ee/api/repository_files.html
  */
@@ -9,6 +9,7 @@ const filePaths = [
   'data/about.json',
   'data/contact.json',
   'data/impressum.json',
+  'data/home.json',
   'data/site.json',
 ]
 
--- src/components/Paragraph/index.tsx
+++ src/components/Paragraph/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, ReactNode } from 'react'
 import styled from 'styled-components'
 import { Animation } from './components/Animation'
 import { isServer } from '../../utils/ssr'
@@ -7,22 +7,31 @@ const StyledParagraph = styled.p`
   margin-bottom: 1rem;
 `
 
-interface ParagraphProps {
-  text: string
-  animate?: boolean
+type CommonProps = {
+  isContent?: boolean
 }
+
+type TruncateProps =
+  | { children: ReactNode; text?: never; animate?: never }
+  | { children?: never; text: string; animate?: boolean }
+
+type ParagraphProps = CommonProps & TruncateProps
 
 const Paragraph: FunctionComponent<ParagraphProps> = ({
   text,
   animate = false,
+  isContent = false,
+  children,
 }) => {
   return (
-    <StyledParagraph>
-      {animate && !isServer()
+    <StyledParagraph data-content={isContent}>
+      {animate && text && !isServer()
         ? text
             .split(' ')
             .map((word, index) => <Animation key={index} text={word} />)
-        : text}
+        : text
+        ? text
+        : children}
     </StyledParagraph>
   )
 }
