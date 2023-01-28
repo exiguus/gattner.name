2047316bfe5261ec6c32b6811be612c0bd8a49a6
Simon Gattner <exiguus@users.noreply.github.com>
2023-01-28 12:23:44 +0100

    fix(loading): content suspense (#33)
    * fix(loading): content suspense
    
    * fix(loading): content suspense add height
    
    ---------
    
    Co-authored-by: Simon Gattner <github@0x38.de>

---
+++ apps/corporate/src/components/Loading/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  min-height: 18rem;
+`
+
+export const Loading = () => <StyledContainer></StyledContainer>
