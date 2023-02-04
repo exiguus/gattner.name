6dc5157d8f9cde0ef5d31d1b32ca9c2779994f0d
Simon Gattner <github@0x38.de>
2023-02-04 02:05:23 +0100

    feat(track): support sw and browser

--- apps/corporate/src/lib/tracker.ts
+++ apps/corporate/src/lib/tracker.ts
@@ -1,19 +1,45 @@
 import { isActionValue, Action } from '@gattner/tracker'
-import { BrowserFingerprint } from '@gattner/utils'
+import { BrowserFingerprint, MurmurHash3, waitFor } from '@gattner/utils'
+import { isServiceWorker } from '../utils/serviceworker'
 import { isPrerender } from '../utils/prerender'
+import { pushActions } from '../sw-tracker'
 
-const browserFingerprint = isPrerender() ? null : new BrowserFingerprint()
-const fingerprint = browserFingerprint ? browserFingerprint.result() : 1
+// self refers to ServiceWorkerGlobalScope instead of window
+//  origin: https://github.com/microsoft/TypeScript/issues/14877#issuecomment-493729050
+declare let self: ServiceWorkerGlobalScope
 
-const append = {
+const appendBase = {
   pid: 1,
   name: 'gattner-corporate',
+  url: '',
+  navigator: '',
+  origin: '',
+  referrer: '',
+  fingerprint:
+    typeof window !== 'undefined' && !isPrerender()
+      ? new BrowserFingerprint().result()
+      : // self refers to ServiceWorkerGlobalScope instead of window
+        //  also this is the reason why we need to use a different fingerprint for the service worker
+        //  the service worker does not have access to the window
+        //  therefore we have to find a way to generate a fingerprint for the service worker and the window
+        new MurmurHash3(self.serviceWorker.scriptURL).result(),
+}
+
+const generateAppend = () => ({
+  ...appendBase,
   url: window.location.href,
   navigator: `${navigator.userAgent || navigator.appVersion}`,
   origin: window.origin || document.location.origin,
   referrer: document.referrer,
-  fingerprint,
-}
+})
+
+const generateAppendSW = () => ({
+  ...appendBase,
+  url: self.location.href,
+  navigator: `${self.navigator.userAgent || self.navigator.appVersion}`,
+  origin: self.location.origin,
+  referrer: self.serviceWorker.scriptURL,
+})
 
 export function trackBindSendEvent() {
   if (isPrerender()) return
@@ -28,16 +54,47 @@ export function trackBindSendEvent() {
 }
 
 export const track = (...args: Array<Action['value']>) => {
-  if (isPrerender() || !('sw' in window)) return
-  const actions = new Set<Action>()
+  if (args.length === 0 || isPrerender()) return
   if (args.every(isActionValue)) {
-    args.forEach(action => {
-      const timestamp = Date.now()
-      actions.add({ key: { timestamp }, value: action, append })
-    })
+    waitFor(() => isServiceWorker(), 'trackerAddActions', 30)
+      .then(async res => {
+        await res
+        return isServiceWorker()
+          ? sendPushActions(args)
+          : sendPushActionsSW(args)
+      })
+      .catch(error => {
+        if (process.env.NODE_ENV === 'development')
+          console.log('trackerAddActions', error)
+      })
   }
-  window.sw.messageSW({
-    type: 'TRACKER_PUSH_ACTIONS',
-    actions: Array.from(actions),
+}
+
+const sendPushActions = (actions: Array<Action['value']>) => {
+  const actionItems = new Set<Action>()
+  actions.forEach(action => {
+    const timestamp = Date.now()
+    actionItems.add({
+      key: { timestamp },
+      value: action,
+      append: generateAppend(),
+    })
   })
+  return window.sw.messageSW({
+    type: 'TRACKER_PUSH_ACTIONS',
+    actions: Array.from(actionItems),
+  })
+}
+
+const sendPushActionsSW = (actions: Array<Action['value']>) => {
+  const actionItems = new Set<Action>()
+  actions.forEach(action => {
+    const timestamp = Date.now()
+    actionItems.add({
+      key: { timestamp },
+      value: action,
+      append: generateAppendSW(),
+    })
+  })
+  return pushActions(Array.from(actionItems))
 }
--- apps/corporate/src/sw-tracker.ts
+++ apps/corporate/src/sw-tracker.ts
@@ -4,28 +4,33 @@ import { isObject } from '@gattner/utils'
 
 const trackerInstance = new Tracker({
   api: new Api(),
-  count: 3,
+  count: 8,
   factor: 1.666,
   max: 23,
   debounce: 12000,
 })
+
+export const pushActions = (actions: unknown) => {
+  if (Array.isArray(actions) && actions.every(isAction)) {
+    actions.forEach(action => {
+      trackerInstance.push(action)
+    })
+  }
+}
+
+export const sendActions = () => {
+  trackerInstance.send()
+}
 
 export const tracker = (self: ServiceWorkerGlobalScope) => {
   self.addEventListener('message', event => {
     if (isObject(event.data) && typeof event.data.type === 'string') {
       switch (event.data.type) {
         case 'TRACKER_PUSH_ACTIONS':
-          if (
-            Array.isArray(event.data.actions) &&
-            event.data.actions.every(isAction)
-          ) {
-            event.data.actions.forEach(action => {
-              trackerInstance.push(action)
-            })
-          }
+          pushActions(event.data.actions)
           break
         case 'TRACKER_SEND_ACTIONS':
-          trackerInstance.send()
+          sendActions()
           break
       }
     }
