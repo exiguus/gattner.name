7547157372569186690c921b6e509c0898a128c6
Simon Gattner <github@0x38.de>
2023-03-08 00:39:39 +0100

    test(page): meta informations

--- apps/corporate/playwright.config.ts
+++ apps/corporate/playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 4,
+  workers: process.env.CI ? 3 : 6,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -78,8 +78,8 @@ export default defineConfig({
   // outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'pnpx http-server dist',
-    port: 8080,
-  },
+  // webServer: {
+  //   command: 'pnpx http-server dist',
+  //   port: 8080,
+  // },
 })
