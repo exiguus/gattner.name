c9c4e90e515df1eff9a7a2479686ee93e818b6b7
Simon Gattner <github@0x38.de>
2023-03-07 03:37:13 +0100

    feat(error): add description

--- apps/corporate/src/pages/Error/index.tsx
+++ apps/corporate/src/pages/Error/index.tsx
@@ -6,21 +6,28 @@ import { Headline } from '../../components/Headline'
 import { Paragraph } from '../../components/Paragraph'
 import { HorizontalBreak } from '../../components/HorizontalBreak'
 
-const Error: FunctionComponent<ErrorProps> = ({ content, quote, title }) => {
+const Error: FunctionComponent<ErrorProps> = ({
+  content,
+  quote,
+  title,
+  description,
+}) => {
   const currentQuote = quote.content[getRandomInt(0, quote.content.length)]
   useEffect(() => {
-    import('../../lib/tracker').then(({ track }) => {
-      track({
-        type: 'load',
-        msg: 'Error loaded',
-        value: `Error Page loaded at location "${window.location.href} with Quote "${currentQuote}"`,
+    if (currentQuote)
+      import('../../lib/tracker').then(({ track }) => {
+        track({
+          type: 'load',
+          msg: 'Error loaded',
+          value: `Error Page loaded at location "${window.location.href} with Quote "${currentQuote}"`,
+        })
       })
-    })
-  }, [])
+  }, [currentQuote])
 
   return (
     <>
       <Headline>{title}</Headline>
+      <Paragraph>{description}</Paragraph>
       <Blockquote author={quote.author} cite={quote.cite}>
         <Paragraph text={currentQuote} />
       </Blockquote>
